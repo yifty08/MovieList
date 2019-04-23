c3db7b7f662570b2c0d3d4d98e61351b33bc151b
Yifty <yifty08@icloud.com>
2019-04-23 16:16:24 -0400

    Print out the list of movies sorted in alphabetical order.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
@@ -10,27 +11,40 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        String anwser = "";
+        String answer = "";
 
 //        do {
 //            System.out.print("Enter your favorite movie: ");
 //            String movieName = scanner.nextLine();
 //            System.out.println("Your fav movie is " + movieName);
 //            System.out.println("Enter another movie? (enter \"q\" to quit.)");
-//            anwser = scanner.nextLine();
-//        } while(!anwser.equalsIgnoreCase("q"));
+//            answer = scanner.nextLine();
+//        } while(!answer.equalsIgnoreCase("q"));
 
         System.out.print("Would you like to share your favorite movie with the world? ");
-        anwser = scanner.nextLine();
+        answer = scanner.nextLine();
 
-        while (!(anwser.equalsIgnoreCase("q") || anwser.equalsIgnoreCase("no"))) {
+        while (!(answer.equalsIgnoreCase("q") || answer.equalsIgnoreCase("no"))) {
 
             System.out.print("Enter your favorite movie: ");
+//            Movie movie = new Movie();
+//            movie.setMovie(scanner.nextLine());
             String movieName = scanner.nextLine();
-            System.out.println("Your fav movie is " + '\"' + movieName + '\"' + ".");
-            System.out.print("Enter another movie?");
-            anwser = scanner.nextLine();
+            movies.add(movieName);
 
+            System.out.println("Your fav movie is " + '\"' + movieName + '\"' + ".");
+            System.out.print("Enter another movie? ");
+            answer = scanner.nextLine();
+
+            if (answer.equalsIgnoreCase("q") || answer.equalsIgnoreCase("no")) {
+                Collections.sort(movies);
+                for ( String movie : movies) {
+                    System.out.println(movie);
+                }
+
+            }
         }
+
+
     }
 }
